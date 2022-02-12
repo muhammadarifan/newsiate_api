a6392a1fa5fae3b69d18f2b6343c77498596474a
arifan <arifmuhammad.idn@gmail.com>
2022-02-12 12:29:14 +0700

    update news-category get

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config/app');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var newsCategoryRouter = require('./routes/newscategory');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/news-category', newsCategoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +32,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = config.app.app_debug == true ? err : {};
 
   // render the error page
   res.status(err.status || 500);
